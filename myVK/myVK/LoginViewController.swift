ba8d8d68ae727c3eba322e9bcfd33f55b3adee45
Polyakov Slava <pslav72@gmail.com>
2021-02-13 21:00:28 +0500

    Check lessons 01

---
+++ myVK/myVK/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myVK
+//
+//  Created by Вячеслав Поляков on 11.02.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
