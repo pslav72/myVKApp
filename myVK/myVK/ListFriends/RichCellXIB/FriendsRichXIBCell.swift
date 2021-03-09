b0c72c8730e655abe8066cbd7d4e4041c31af4c1
Polyakov Slava <pslav72@gmail.com>
2021-03-09 21:29:21 +0500

    Replace buttom to ImageView

--- myVK/myVK/ListFriends/RichCellXIB/FriendsRichXIBCell.swift
+++ myVK/myVK/ListFriends/RichCellXIB/FriendsRichXIBCell.swift
@@ -12,16 +12,54 @@ class FriendsRichXIBCell: UITableViewCell {
     static let reuseIdentifier = "FriendsRichXIBCell"
     static let nibName = "FriendsRichXIBCell"
     
-    @IBOutlet var friendsImageButton: UIButton! {
+    private lazy var imageFriends​TapGestureRecognizer: UITapGestureRecognizer = {
+        let imageFriendsTapRecogniser = UITapGestureRecognizer(target: self, action: #selector(actionTapImageFriends))
+        imageFriendsTapRecogniser.numberOfTapsRequired = 1
+        imageFriendsTapRecogniser.numberOfTouchesRequired = 1
+        return imageFriendsTapRecogniser
+    } ()
+            
+    
+    @IBOutlet var friendsImageButton: UIImageView! {
         didSet {
             self.friendsImageButton.layer.backgroundColor = UIColor.systemFill.cgColor
         }
     }
     @IBOutlet var friendsShadowView: UIView!
     @IBOutlet var friendsNameLabel: UILabel!
-    
-    @IBAction func buttonPressedFriendsImage() {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        friendsImageButton.clipsToBounds = true
+        friendsShadowView.layer.shadowColor = UIColor.systemBlue.cgColor
+        friendsShadowView.layer.shadowRadius = 3
+        friendsShadowView.layer.shadowOpacity = 0.8
+        friendsImageButton.isUserInteractionEnabled = true
+        friendsImageButton.addGestureRecognizer(imageFriends​TapGestureRecognizer)
         
+    }
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        friendsImageButton.layer.cornerRadius = friendsImageButton.bounds.width/2
+        friendsShadowView.layer.cornerRadius = friendsShadowView.bounds.width/2
+    }
+
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    
+    public func configure(with friends: User) {
+        friendsNameLabel.text = friends.name
+        friendsImageButton.image = friends.image
+    }
+    
+    
+    @objc func actionTapImageFriends() {
         UIView.animate(withDuration: 3, delay: 0, options: [.curveEaseInOut]) {
             [self] in
             let affineScaleX: CGFloat = 0.2
@@ -35,32 +73,4 @@ class FriendsRichXIBCell: UITableViewCell {
             }
         }
     }
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        friendsImageButton.clipsToBounds = true
-        friendsShadowView.layer.shadowColor = UIColor.systemBlue.cgColor
-        friendsShadowView.layer.shadowRadius = 3
-        friendsShadowView.layer.shadowOpacity = 0.8
-        
-        
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        friendsImageButton.layer.cornerRadius = friendsImageButton.bounds.width/2
-        friendsShadowView.layer.cornerRadius = friendsShadowView.bounds.width/2
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-    }
-    
-    public func configure(with friends: User) {
-        friendsNameLabel.text = friends.name
-        friendsImageButton.setBackgroundImage(friends.image, for: [])
-        
-    }
-    
 }
--- myVK/myVK/ListGorups/RichCellXIB/GroupsRichXIBCell.swift
+++ myVK/myVK/ListGorups/RichCellXIB/GroupsRichXIBCell.swift
@@ -12,30 +12,25 @@ class GroupsRichXIBCell: UITableViewCell {
     static let reuseIdentifier = "GroupsRichXIBCell"
     static let nibName = "GroupsRichXIBCell"
     
-    @IBOutlet var groupsButtomImage: UIButton! {
+    @IBOutlet var groupsImageView: UIImageView! {
         didSet {
-            self.groupsButtomImage.layer.backgroundColor = UIColor.systemFill.cgColor
+            self.groupsImageView.layer.backgroundColor = UIColor.systemFill.cgColor
         }
     }
     @IBOutlet var groupsNameLabel: UILabel!
     
-    @IBAction func buttonPressedFriendsImage() {
-        
-        UIView.animate(withDuration: 3, delay: 0, options: [.curveEaseInOut]) {
-            [self] in
-            let affineScaleX: CGFloat = 0.2
-            let affineScaleY: CGFloat = 0.2
-            groupsButtomImage.transform = CGAffineTransform(scaleX: affineScaleX, y: affineScaleY)
-        } completion: { _ in
-            UIView.animate(withDuration: 3, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: []) {
-                self.groupsButtomImage.transform = .identity
-            }
-        }
-    }
+    private lazy var groupsImageViewTapGestureRecognizer: UITapGestureRecognizer = {
+        let groupsImageViewTapRecogniser = UITapGestureRecognizer(target: self, action: #selector(actionTapGroupsImageView))
+        groupsImageViewTapRecogniser.numberOfTapsRequired = 1
+        groupsImageViewTapRecogniser.numberOfTouchesRequired = 1
+        return groupsImageViewTapRecogniser
+    } ()
+    
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        groupsImageView.isUserInteractionEnabled = true
+        groupsImageView.addGestureRecognizer(groupsImageViewTapGestureRecognizer)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -46,7 +41,20 @@ class GroupsRichXIBCell: UITableViewCell {
     
     public func configure(with groups: Group) {
         groupsNameLabel.text = groups.name
-        groupsButtomImage.setBackgroundImage(groups.image, for: [])
+        groupsImageView.image = groups.image
     }
     
+    
+    @objc func actionTapGroupsImageView() {
+        UIView.animate(withDuration: 3, delay: 0, options: [.curveEaseInOut]) {
+            [self] in
+            let affineScaleX: CGFloat = 0.2
+            let affineScaleY: CGFloat = 0.2
+            groupsImageView.transform = CGAffineTransform(scaleX: affineScaleX, y: affineScaleY)
+        } completion: { _ in
+            UIView.animate(withDuration: 3, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: []) {
+                self.groupsImageView.transform = .identity
+            }
+        }
+    }
 }
